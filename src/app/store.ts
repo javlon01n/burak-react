97b24c3f006451e4af7fe87114ac53e5ae5baf72
MARK <joxa030@gmail.com>
2026-07-18 19:26:41 +0900

    feat: develop slice and selector usage

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,7 +7,7 @@ export const store = configureStore({
    // @ts-ignore
     getDefaultMiddleware().concat(reduxLogger),
   reducer: {
-    placeholder: HomePageReducer,
+    homePage: HomePageReducer,
   },
 });
 
