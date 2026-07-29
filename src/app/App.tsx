91a043412c63cfbfed13c42f0645d1d2b7b62242
MARK <joxa030@gmail.com>
2026-07-30 05:16:59 +0900

    fix: modiyf basket business logic via customized hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,48 +8,36 @@ import  HomeNavbar  from "./components/headers/HomeNavbar";
 import  OtherNavbar  from "./components/headers/OtherNavbar";
 import  Footer  from "./components/footer/";
 import  HelpPage  from "./screens/helpPage";
-import { useState } from "react";
-import { CartItem } from "../lib/types/search";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import useBasket from "./components/hooks/useBasket";
 
 
     function App() {
   const location = useLocation();
+  const {cartItems, onAdd,  onRemove, onDelete, onDeleteAll, } = useBasket();
 
-  const cartJson: string | null = localStorage.getItem("cartData");
-  const currentCart = cartJson ? JSON.parse(cartJson) : [];
-  const [cartItems, setCartItems] = useState<CartItem[]>(currentCart);
 
-  /** HANDLERS */
-
-  const onAdd = (input: CartItem) => {
-    const exist: any = cartItems.find(
-      (item: CartItem) => {
-            return item._id === input._id;
-        }
-    );
-    if (exist) {
-      const cartUpdate = cartItems.map((item: CartItem) =>
-        item._id === input._id
-          ? { ...exist, quantity: exist.quantity + 1 }
-          : item
-      );
-      setCartItems(cartUpdate);
-      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    } else {
-      const cartUpdate = [...cartItems, { ...input }];
-      setCartItems(cartUpdate);
-      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    }
-  };
+ 
 
   return (
    <>
     {location.pathname === "/" ? 
-    <HomeNavbar cartItems={cartItems} /> : 
-    <OtherNavbar cartItems={cartItems} />}
+    <HomeNavbar 
+    cartItems={cartItems} 
+    onAdd={onAdd}
+    onRemove={onRemove} 
+    onDelete={onDelete} 
+    onDeleteAll={onDeleteAll} 
+    /> : 
+    <OtherNavbar 
+    cartItems={cartItems}
+    onAdd={onAdd}
+     onRemove={onRemove} 
+     onDelete={onDelete} 
+     onDeleteAll={onDeleteAll} 
+     />}
     <Switch>
      <Route path="/products">
           <ProductsPage onAdd={onAdd} />
